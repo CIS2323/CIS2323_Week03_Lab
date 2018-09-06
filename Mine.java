3d126d2c72e4617daa6b61bb7fba25984447fc76
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-05 19:55:34 -0500

    Adding getters and setters too our new Lamp class

--- Mine.java
+++ Mine.java
@@ -1,9 +1,22 @@
 import mystuff.*;
+import lab2.*;
 
 public class Mine {
 	public static void main(String[] args){
-		System.out.println("This is mine!");
+		Lamp myLamp = new Lamp();
+		System.out.println("This is my lamp!");
+		
+		myLamp.setColor("white");
+		myLamp.setWatts(45);
+		myLamp.turnOn();
+		
+		String status = myLamp.showStatus();
+		System.out.println(status);
+
+		Lamp yourLamp = new Lamp();
 		Yours.print();
+		
+		Lamp otherLamp = new Lamp();
 		Others.print();
 	}
 }
